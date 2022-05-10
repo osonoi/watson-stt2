9a5415dfae2905936695de50679cc8132d0402b6
小薗井康志 <shousonoiyasushi@shousonoiyasushinoMacBook-Pro.local>
2022-05-10 20:58:50 +0900

    スペースを取り除くコード追加

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ filename = 'uploads/' + req.file.originalname;
       console.log(r.alternatives[0].transcript);
       msg = msg + r.alternatives[0].transcript;
       };
+      msg = msg.replace(/\s+/g, "");
       msg = msg + '<br>' + '検索ワードは' + search_word;
       var data = {
           title: 'Text',
